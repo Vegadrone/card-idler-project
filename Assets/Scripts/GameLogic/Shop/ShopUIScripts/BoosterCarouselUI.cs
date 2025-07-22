13a1b878860a283a2eb338022d153952682b3bf8
Vegadrone <antonino.spena.dev@gmail.com>
2025-07-22 18:08:09 +0200

    feat: :sparkles: Add Feel package from asset store, make cards from opened pack visible after the purchase

--- Assets/Scripts/GameLogic/Shop/ShopUIScripts/BoosterCarouselUI.cs
+++ Assets/Scripts/GameLogic/Shop/ShopUIScripts/BoosterCarouselUI.cs
@@ -19,6 +19,9 @@ public class BoosterCarouselUI : MonoBehaviour
     [SerializeField] private Button rightArrowButton;
     [SerializeField] private Button openBoosterButton;
 
+    [Header("Opened Cards UI")]
+    [SerializeField] private Transform openedCardsContainer;
+    [SerializeField] private GameObject cardUIPrefab;
 
     private List<BoosterPackSO> availableBoosters;
     private SetDatabaseSO setDatabase;
@@ -100,6 +103,30 @@ public class BoosterCarouselUI : MonoBehaviour
     
     public void ShowOpenedCards(List<CardData> cards)
     {
+        if (openedCardsContainer == null)
+    {
+        Debug.LogError("[BoosterCarouselUI] openedCardsContainer is not assigned.");
+        return;
+    }
+        foreach (Transform child in openedCardsContainer)
+        {  
+            Destroy(child.gameObject);  
+        }
+
+        foreach (var cardData in cards)
+        {
+            GameObject cardGO = Instantiate(cardUIPrefab, openedCardsContainer);
+
+            CardDisplayer cardDisplayer = cardGO.GetComponent<CardDisplayer>();
+            if (cardDisplayer != null)
+            {
+                cardDisplayer.DisplayCard(cardData, assetsLoader);
+            }
+            else
+            {
+                Debug.LogWarning("[BoosterCarouselUI] - Instantiated card prefab is missing CardDisplayer component");
+            }
+        }
         Debug.Log("[BoosterCarouselUI] - Showing opened cards:");
         foreach (var card in cards)
         {
